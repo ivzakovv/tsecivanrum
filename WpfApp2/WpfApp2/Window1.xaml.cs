9286737d34116d906dfc4dbe78a79957ec4ded70
ivzakovv <85224144+ivzakovv@users.noreply.github.com>
2021-06-02 13:13:35 +0400

    Add files via upload

---
+++ WpfApp2/WpfApp2/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WpfApp2.aeroDataSet aeroDataSet = ((WpfApp2.aeroDataSet)(this.FindResource("aeroDataSet")));
+            // Загрузить данные в таблицу aero. Можно изменить этот код как требуется.
+            WpfApp2.aeroDataSetTableAdapters.aeroTableAdapter aeroDataSetaeroTableAdapter = new WpfApp2.aeroDataSetTableAdapters.aeroTableAdapter();
+            aeroDataSetaeroTableAdapter.Fill(aeroDataSet.aero);
+            System.Windows.Data.CollectionViewSource aeroViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("aeroViewSource")));
+            aeroViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void AeroDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Window2.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (logzareg.Text.Length > 0)
+            {
+                if (parolzareg.Text.Length > 0)
+                {
+                    if (povtorzaregpar.Text.Length > 0) {
+                        if (povtorzaregpar.Text == parolzareg.Text)
+                        {
+                            MainWindow zllp = new MainWindow();
+                            zllp.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверный повтор пароля", "ошибка ", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Повторите пароль", "ошибка ", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Введите пароль", "ошибка ", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин", "ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
